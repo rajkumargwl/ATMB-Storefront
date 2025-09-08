63b7afaf6bf0006f296283376384d21b6f62c31c
shreya <shree.gupta617@gmail.com>
2025-09-08 16:47:45 +0530

    home page design

---
+++ app/components/icons/Testimonial.tsx
@@ -0,0 +1,16 @@
+import {SVGAttributes} from 'react';
+
+const Testimonial: React.FC = (props: SVGAttributes<SVGElement>) => (
+    <svg width="22" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_484_1647)">
+    <path d="M0.333008 11.0009V20.4294H9.76159V11.0009H3.47589C3.47589 7.53498 6.29572 4.71515 9.76159 4.71515V1.57227C4.56236 1.57227 0.333008 5.80162 0.333008 11.0009ZM22.333 4.71515V1.57227C17.1338 1.57227 12.9044 5.80162 12.9044 11.0009V20.4294H22.333V11.0009H16.0473C16.0473 7.53498 18.8671 4.71515 22.333 4.71515Z" fill="#EE6D2D"/>
+    </g>
+    <defs>
+    <clipPath id="clip0_484_1647">
+    <rect width="22" height="22" fill="white" transform="translate(0.333008)"/>
+    </clipPath>
+    </defs>
+    </svg>
+);
+
+export default Testimonial;
