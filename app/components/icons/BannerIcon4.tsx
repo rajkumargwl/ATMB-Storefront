92a92e6641a2f024e7c6617e63ebe36324217ff0
sejal jain <sejal.jain@galaxyweblinks.in>
2025-09-16 15:28:53 +0530

    merge

---
+++ app/components/icons/BannerIcon4.tsx
@@ -0,0 +1,11 @@
+import {SVGAttributes} from 'react';
+
+const BannerIcon4: React.FC = (props: SVGAttributes<SVGElement>) => (
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M7.12498 16.0986H5.62499C2.91796 16.0986 0.703125 13.8838 0.703125 11.1768C0.703125 8.46975 2.91796 6.25491 5.62499 6.25491H5.81197C6.42237 3.39784 8.96074 1.25515 12 1.25515C15.0392 1.25515 17.5776 3.39784 18.188 6.25491H18.3749C21.082 6.25491 23.2968 8.46975 23.2968 11.1768C23.2968 13.8838 21.0818 16.0986 18.3749 16.0986H17.6123" stroke="#FF6600" stroke-width="1.5" stroke-miterlimit="22.926" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M12 22.7461C9.25626 22.3917 7.125 20.0468 7.125 17.2223V12.6086L12 10.8865L16.875 12.6086V17.2223C16.875 20.0468 14.7438 22.3917 12 22.7461Z" stroke="#FF6600" stroke-width="1.5" stroke-miterlimit="22.926" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M10.4795 16.8867L11.5393 17.9414L13.5176 15.9727" stroke="#FF6600" stroke-width="1.5" stroke-miterlimit="22.926" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+);
+
+export default BannerIcon4;
