dec46d54da2c05abd36d247a9743279f34162199
shreya <shree.gupta617@gmail.com>
2025-09-17 11:34:25 +0530

    home page design

---
+++ app/components/icons/CloseFAQ.tsx
@@ -0,0 +1,10 @@
+import {SVGAttributes} from 'react';
+
+const CloseFAQ: React.FC = (props: SVGAttributes<SVGElement>) => (
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M6 18L18 6" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M18 18L6 6" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+);
+
+export default CloseFAQ;
