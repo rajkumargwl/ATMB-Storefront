65e90508f4855404fdff5548f9becaad1dc6ecec
sejal jain <sejal.jain@galaxyweblinks.in>
2025-09-17 16:47:36 +0530

    dynamic queries

--- app/queries/sanity/fragments/modules/homeSection3.ts
+++ app/queries/sanity/fragments/modules/homeSection3.ts
@@ -1,35 +1,73 @@
-import groq from 'groq';
+// import groq from 'groq';
 
-import {IMAGE_WITH_PRODUCT_HOTSPOTS} from '../imageWithProductHotspots';
-import {LINK_EXTERNAL} from '../linkExternal';
-import {LINK_INTERNAL} from '../linkInternal';
-import {PRODUCT_WITH_VARIANT} from '../productWithVariant';
-import {IMAGE} from '../image';
+// import {IMAGE_WITH_PRODUCT_HOTSPOTS} from '../imageWithProductHotspots';
+// import {LINK_EXTERNAL} from '../linkExternal';
+// import {LINK_INTERNAL} from '../linkInternal';
+// import {PRODUCT_WITH_VARIANT} from '../productWithVariant';
+// import {IMAGE} from '../image';
+
+// export const HOME_SECTION_3 = groq`
+//     title,
+//     subtitle1,
+//     subtitle2,
+//     tabs[] {
+//       label,
+//       description,
+//       keyNeeds[],
+//       services[] {
+//         service,
+//         icon {
+//            ${IMAGE}
+//         },
+//         title,
+//         description,
+//         link {
+//           ...LINK_INTERNAL,
+//           ...LINK_EXTERNAL
+//         },
+//       },
+//       button,
+//       image {
+//          ${IMAGE}
+//       },
+//       quote,
+//     }
+// `;
+import groq from 'groq';
+import { IMAGE } from '../image';
+import { LINK_EXTERNAL } from '../linkExternal';
+import { LINK_INTERNAL } from '../linkInternal';
 
 export const HOME_SECTION_3 = groq`
-    title,
-    subtitle1,
-    subtitle2,
-    tabs[] {
+  title,
+  subtitle,
+  tabs[] {
+    label,
+    heading,
+    description,
+    keyNeeds[],
+    quote {
+      avatar {
+        ${IMAGE}
+      },
+      author,
+      text
+    },
+    services[] {
+      icon {
+        upload {
+          ${IMAGE}
+        },
+        svgCode
+      },
+      title,
+      description
+    },
+    button {
       label,
-      description,
-      keyNeeds[],
-      services[] {
-        service,
-        icon {
-           ${IMAGE}
-        },
-        title,
-        description,
-        link {
-          ...LINK_INTERNAL,
-          ...LINK_EXTERNAL
-        },
-      },
-      button,
-      image {
-         ${IMAGE}
-      },
-      quote,
+      textColor,
+      bgColor
     }
+  }
 `;
+
--- app/queries/sanity/fragments/pages/home.ts
+++ app/queries/sanity/fragments/pages/home.ts
@@ -1,13 +1,11 @@
 import groq from 'groq';
 
-import {HERO_HOME} from '../heroes/home';
+// import {MODULE_HERO_BANNER} from '../modules/herobanner';
 import {MODULES} from '../modules';
 import {SEO} from '../seo';
 
 export const HOME_PAGE = groq`
-  hero {
-    ${HERO_HOME}
-  },
+ 
   modules[] {
     ${MODULES}
   },
