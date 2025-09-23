838f6513632da6d71be5a40988efd5e809ec439b
Anshu-Shar <anshu.sharma@galaxyweblinks.in>
2025-09-23 11:47:15 +0530

    Push the blog fronend and backend code

---
+++ shopify-hydrogen/schemaTypes/wpPost.ts
@@ -0,0 +1,50 @@
+export default {
+  name: 'wpPost',
+  title: 'Blogs',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'text',
+    },
+    {
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'datetime',
+    },
+    {
+      name: 'link',
+      title: 'Original Link',
+      type: 'url',
+    },
+    // {
+    //   name: 'author',
+    //   title: 'Author',
+    //   type: 'reference',
+    //   to: [{type: 'author'}], 
+    // },
+  ],
+};
