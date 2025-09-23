d298d579e08cc1f00fc3a2de33f33cbc6cb40a85
shreya <shree.gupta617@gmail.com>
2025-09-23 11:36:26 +0530

    design changes

--- app/components/global/Footer.tsx
+++ app/components/global/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer({ data }: FooterProps) {
         {/* Top Grid */}
         <div className="grid grid-cols-1 lg:grid-cols-5 gap-8 md:gap-5">
           {/* Logo + Title + Description */}
-          <div className="lg:col-span-2 max-w-[306px] flex items-center md:items-start flex-col justify-between gap-6">
+          <div className="lg:col-span-2 md:max-w-[306px] flex items-center md:items-start flex-col justify-between gap-6">
             <div className="block">
               {data.logo?.asset?.url && (
                 <img
