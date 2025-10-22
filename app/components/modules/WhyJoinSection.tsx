a7a8263e0d35078306847b30c9ec561e504c0a1c
ramsharma-09 <ram.contact09@gmail.com>
2025-10-22 20:47:37 +0530

    PDP Pages

--- app/components/modules/WhyJoinSection.tsx
+++ app/components/modules/WhyJoinSection.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Check } from "lucide-react"; // icon for checkmark
 import CheckArrowWhite from "~/components/icons/CheckArrowWhite";
@@ -14,7 +13,7 @@ type WhyJoinSectionProps = {
 export default function WhyJoinSection({ heading, features }: WhyJoinSectionProps) {
   return (
     <section
-      className="w-full px-5 py-10 relative z-[2]"
+      className="w-full px-5 py-10 relative z-[2] overflow-hidden"
       style={{ background: "var(--Background-Black, #091019)" }}
     >
        <div className="absolute hidden md:block z-[1] md:top-[0px] right-[0px] md:right-[0px]">
@@ -23,13 +22,13 @@ export default function WhyJoinSection({ heading, features }: WhyJoinSectionProp
           <div className="absolute block md:hidden z-[1] bottom-[0px] right-[0px]">
         <img src={WhyJoinBgMobile} alt="Background" className="w-[393px] h-[272px]"/>
         </div>
-      <div className="max-w-[1240px] mx-auto">
+      <div className="max-w-[1240px] mx-auto relative z-[2]">
         {heading && (
-          <h2 className="mb-5 md:mb-4 font-Roboto text-white font-medium leading-[28px] md:leading-[28px] text-[20px] md:text-[20px] tracking-[0px]">
+          <h2 className="mb-4 md:mb-4 font-Roboto text-white font-medium leading-[28px] md:leading-[28px] text-[20px] md:text-[20px] tracking-[0px]">
             {heading}
           </h2>
         )}
-        <div className="flex flex-col md:flex-row flex-wrap items-start md:items-center justify-start md:justify-start gap-[15px] md:gap-6">
+        <div className="flex flex-col md:flex-row flex-wrap items-start md:items-center justify-start md:justify-start gap-[16px] md:gap-6">
           {features?.map((feature, idx) => (
             <div key={idx} className="flex items-center gap-3">
               <CheckArrowWhite size={24} />
@@ -43,5 +42,3 @@ export default function WhyJoinSection({ heading, features }: WhyJoinSectionProp
     </section>
   );
 }
- 
- 
