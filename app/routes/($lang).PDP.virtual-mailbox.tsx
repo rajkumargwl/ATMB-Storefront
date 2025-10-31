ad7af10be34eda7d746c88e58d90b188779182fa
rajkumargwl <rajkumar.vishwakarma@galaxyweblinks.in>
2025-10-31 20:59:13 +0530

    services fixed after schema changes

--- app/routes/($lang).PDP.virtual-mailbox.tsx
+++ app/routes/($lang).PDP.virtual-mailbox.tsx
@@ -134,23 +134,24 @@ export default function Plans() {
     )
   );
 
-  // const services = Array.from(
-  //   new Set(
-  //     location.featureList
-  //       .filter((feature) => feature.class !== "HIGHLIGHT")
-  //       .map((feature) => feature.label)
-  //   )
-  // );
-  // this below code is as per the New Schema of Locations 
-  const services = Array.from(
-  new Set(
-    (location.featureList || [])
-      .filter((item) => item.feature?.class !== "HIGHLIGHT")
-      .map((item) => item.feature?.label)
-      .filter(Boolean) // remove undefined/null
-  )
-);
 
+  const services = Array.from(
+    new Set(
+      location.featureList
+        .filter((feature) => feature.class === "Mail")
+        .map((feature) => feature.label)
+    )
+  );
+  // this below code is as per the New Schema of Locations 
+//   const services = Array.from(
+//   new Set(
+//     (location.featureList || [])
+//       .filter((item) => item.feature?.class === "Mail")
+//       .map((item) => item.feature?.label)
+//       .filter(Boolean) // remove undefined/null
+//   )
+// );
+console.log('services:', services);
 
   const variants = (product?.variants?.nodes ?? []) as ProductVariant[];
  
