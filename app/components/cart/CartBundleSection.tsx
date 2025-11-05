b5c855a5a393e9e3584169a14a81b7bd9bbdd405
sejal jain <sejal.jain@galaxyweblinks.in>
2025-11-05 18:56:20 +0530

    Dashboard

--- app/components/cart/CartBundleSection.tsx
+++ app/components/cart/CartBundleSection.tsx
@@ -1,19 +1,19 @@
 import { CartForm, Money } from '@shopify/hydrogen';
 import { DEFAULT_LOCALE } from '~/lib/utils';
 import { useRootLoaderData } from '~/root';
-
+ 
 export default function CartBundleSection({ bundleProducts }: { bundleProducts: any[] }) {
   if (!bundleProducts || bundleProducts.length === 0) return null;
-
+ 
   const selectedLocale = useRootLoaderData()?.selectedLocale ?? DEFAULT_LOCALE;
   let currencyCode = selectedLocale?.currency || 'USD';
   const bundle = bundleProducts[0]; // show only the first bundle
-
+ 
   const displayPrice = bundle.billing === 'monthly' ? bundle.price : bundle.yearlyPrice;
   
   const displayCompare =
     bundle.billing === 'monthly' ? bundle.compareAtPrice : bundle.yearlyCompareAtPrice;
-
+ 
   const savePercentage =
     displayCompare && displayPrice
       ? Math.round(
@@ -21,71 +21,74 @@ export default function CartBundleSection({ bundleProducts }: { bundleProducts: 
             100,
         )
       : null;
-
+ 
   const variantId = bundle.billing === 'monthly' ? bundle.monthlyVariantId : bundle.yearlyVariantId;
-
+ 
   return (
-    <div className="flex flex-col items-center w-[600px] p-6 gap-6 bg-white border border-[#DCDCDC] rounded-[12px]">
+    <div className="flex flex-col items-start w-full lg:w-[50%] gap-[24px] md:gap-10">
       {/* Header */}
-      <div className="flex flex-col items-center text-center gap-2">
+      <div className="flex flex-col items-start text-left gap-1">
         <h2 className="font-Roboto text-[#171717] text-[24px] font-semibold leading-[31.2px] tracking-[-0.36px]">
           Bundle smart and save 15% on the plan
         </h2>
-        <p className="font-Roboto text-[#171717] text-[16px] leading-[24px] font-normal">
+        <p className="font-Roboto text-[#171717] text-[14px] md:text-[16px] leading-[21px] md:ading-[24px] font-normal">
           80% of our customers pick this winning combination
         </p>
       </div>
-
+ 
       {/* Single Bundle Section */}
-      <div className="flex flex-col w-full p-6 gap-4 rounded-[12px] border border-[#DCDCDC] bg-[#FFF] hover:shadow-sm transition">
+      <div className="flex flex-col w-full p-6 gap-[24px] rounded-[12px] border border-[#DCDCDC] bg-[#FFF] transition">
         {/* Main Bundle Row */}
-        <div className="flex justify-between items-start">
+        <div className="flex flex-col md:flex-row justify-between items-start gap-[16px]">
           <div className="flex flex-col gap-1">
-            <h3 className="font-Roboto text-[#171717] text-[20px] font-semibold">
+            <h3 className="font-Roboto text-PrimaryBlack font-semibold leading-[27px] md:leading-[27px] text-[18px] md:text-[18px] tracking-[0px]">
               {bundle.title}
             </h3>
             {savePercentage && (
-              <p className="text-[#74A038] text-[14px] font-Roboto">
+              <p className="font-Roboto text-LightGray font-normal leading-[21px] md:leading-[21px] text-[14px] md:text-[14px] tracking-[0px] ">
                 Save {savePercentage}% with virtual mailbox
               </p>
             )}
           </div>
           <div className="flex flex-col items-end">
-            <div className="flex items-baseline gap-2">
+            <div className="flex items-baseline">
               {displayCompare && (
-                <span className="text-[#9CA3AF] line-through text-[14px]"> <Money data={{ amount: displayCompare, currencyCode: currencyCode }}/></span>
+                <span className="pr-2 line-through font-Roboto text-PrimaryBlack font-semibold leading-[27px] md:leading-[27px] text-[18px] md:text-[18px] tracking-[0px]"> <Money data={{ amount: displayCompare, currencyCode: currencyCode }}/></span>
               )}
-              <span className="text-[#171717] text-[20px] font-semibold"><Money data={{ amount: displayPrice, currencyCode: currencyCode }}/></span>
-              <span className="text-[#9CA3AF] text-[14px]">
+              <span className="font-Roboto text-PrimaryBlack font-semibold leading-[31.2px] md:leading-[31.2px] text-[24px] md:text-[24px] tracking-[-0.3px] md:tracking-[-0.36px]"><Money data={{ amount: displayPrice, currencyCode: currencyCode }}/></span>
+              <span className="font-Roboto text-LightGray font-normal leading-[21px] text-[14px] tracking-[0px]">
                 /{bundle.billing === 'monthly' ? 'month' : 'year'}
               </span>
             </div>
           </div>
         </div>
-
+ 
         {/* Associated Items - Inline Scrollable Row */}
         {bundle.associatedItems?.length > 0 && (
-          <div className="flex overflow-x-auto flex-nowrap gap-4 mt-2 pb-2 scrollbar-thin scrollbar-thumb-gray-300">
+          <div className="flex flex-col md:flex-row gap-[24px]">
             {bundle.associatedItems.map((item, idx) => (
               <div
                 key={idx}
-                className="flex-shrink-0 flex flex-col justify-between p-4 w-[180px] rounded-[12px] border border-[#DCDCDC] bg-[#F9FAFB] hover:shadow-md transition"
+                className="w-full md:w-[50%] flex flex-col p-5 rounded-[12px] border border-[#DCDCDC] bg-[#FFF]"
               >
                 <div>
-                  <span className="font-semibold text-[#171717] text-[14px] block mb-1">
+                  <span className=" font-Roboto text-PrimaryBlack font-semibold leading-[28px] md:leading-[28px] text-[20px] md:text-[20px] tracking-[-0.3px] md:tracking-[-0.3px] block mb-2">
                     {item.productTitle}
                   </span>
                   {item.price && (
-                    <span className="flex text-[#9CA3AF] text-[13px]">  
+                    <span className="flex font-Roboto text-PrimaryBlack font-normal leading-[18px] text-[12px] tracking-[0px]">  
                     <Money data={{ amount: item.price, currencyCode: currencyCode }}/>/m</span>
                   )}
                 </div>
-
+ 
                 {item.features && item.features.length > 0 && (
-                  <ul className="mt-2 mb-2 space-y-1 text-[13px] text-[#333333]">
+                  <ul className="mt-[24px] space-y-4">
                     {item.features.map((f: string, fIdx: number) => (
-                      <li key={fIdx} className="flex items-center gap-1">
-                        <span className="text-[#74A038] text-sm">✓</span>
+                      <li key={fIdx} className="flex items-center gap-3 font-Roboto text-PrimaryBlack font-normal leading-[21px] md:leading-[21px] text-[14px] md:text-[14px] tracking-[0px]">
+                        <span className="flex items-center justify-center w-[24px] h-[24px]">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none"><path d="M16.5544 0.110975C16.8206 0.305975 16.8806 0.680975 16.6856 0.950975L6.18563 15.351C6.08063 15.4935 5.92313 15.5835 5.74688 15.5947C5.57063 15.606 5.40188 15.546 5.27438 15.4222L0.174375 10.3222C-0.058125 10.0897 -0.058125 9.70722 0.174375 9.47472C0.406875 9.24222 0.789375 9.24222 1.02188 9.47472L5.62688 14.0797L15.7144 0.245975C15.9094 -0.0202754 16.2844 -0.0802754 16.5544 0.114725V0.110975Z" fill="#091019"></path>
+                          </svg>
+                        </span>
                         {f}
                       </li>
                     ))}
@@ -95,8 +98,9 @@ export default function CartBundleSection({ bundleProducts }: { bundleProducts: 
             ))}
           </div>
         )}
-
+ 
         {/* Add to Cart Button */}
+        <div className="flex items-center justify-center">
         <CartForm
           action={CartForm.ACTIONS.LinesAdd}
           inputs={{
@@ -107,12 +111,13 @@ export default function CartBundleSection({ bundleProducts }: { bundleProducts: 
             <button
               type="submit"
               disabled={props.state !== 'idle'}
-              className="flex items-center justify-center w-full h-[44px] rounded-[100px] border border-[#171717] text-[#171717] text-[16px] font-normal tracking-[0.08px] hover:bg-[#171717] hover:text-white transition"
+              className="flex items-center justify-center gap-[12px] w-full md:w-[202px] h-[44px] rounded-[100px] font-normal leading-[16px] tracking-[0.08px] text-[16px] text-PrimaryBlack border border-[#091019] px-4 py-[12px]"
             >
               {props.state !== 'idle' ? 'Adding...' : 'Upgrade to Bundle'}
             </button>
           )}
         </CartForm>
+        </div>
       </div>
     </div>
   );
--- sanity-client/schemaTypes/news.ts
+++ sanity-client/schemaTypes/news.ts
@@ -1,15 +1,73 @@
+// import { GROUPS } from '../../constants'
+// export default {
+//   name: 'news',
+//   title: 'Newsroom',
+//   type: 'document',
+//   fields: [
+//     {
+//       name: 'title',
+//       title: 'Title',
+//       type: 'string',
+//       validation: Rule => Rule.required(),
+//     },
+//     {
+//       name: 'slug',
+//       title: 'Slug',
+//       type: 'slug',
+//       options: {
+//         source: 'title',
+//         maxLength: 96,
+//       },
+//       validation: Rule => Rule.required(),
+//     },
+//     {
+//       name: 'description',
+//       title: 'Description',
+//       type: 'blockContent',
+//     },
+//     {
+//       name: 'featuredImage',
+//       title: 'Featured Image',
+//       type: 'image',
+//       options: {
+//         hotspot: true,
+//       },
+//     },
+//     {
+//       name: 'logoImage',
+//       title: 'Logo Image',
+//       type: 'image',
+//       options: {
+//         hotspot: true,
+//       },
+//     },
+    
+//     {
+//       name: 'date',
+//       title: 'Publish Date',
+//       type: 'datetime',
+//       validation: Rule => Rule.required(),
+//     },
+    
+//   ],
+  
+// };
+import { defineField } from 'sanity'
+import { GROUPS } from '../constants'
+
 export default {
   name: 'news',
   title: 'Newsroom',
   type: 'document',
+  groups: GROUPS, // 👈 this makes use of your GROUPS definition
   fields: [
-    {
+    defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
       validation: Rule => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
@@ -18,33 +76,40 @@ export default {
         maxLength: 96,
       },
       validation: Rule => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'description',
       title: 'Description',
       type: 'blockContent',
-    },
-    {
+    }),
+    defineField({
       name: 'featuredImage',
       title: 'Featured Image',
       type: 'image',
       options: {
         hotspot: true,
       },
-    },
-    {
+    }),
+    defineField({
       name: 'logoImage',
       title: 'Logo Image',
       type: 'image',
       options: {
         hotspot: true,
       },
-    },
-    {
+    }),
+    defineField({
       name: 'date',
       title: 'Publish Date',
       type: 'datetime',
       validation: Rule => Rule.required(),
-    },
+    }),
+    // 👇 Add SEO field group
+    defineField({
+      name: 'seo',
+      title: 'SEO',
+      type: 'seo',
+      group: 'seo',
+    }),
   ],
-};
+}
